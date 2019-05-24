866f8017ae5bbad58e79d3bebf8717387a606f37
Eduardo <hobold.edu@hotmail.com>
2019-05-24 19:27:12 -0300

    Trabalho

---
+++ app/controllers/busca_cep_controller.rb
@@ -0,0 +1,41 @@
+class BuscaCepController < ApplicationController
+    require 'net/http'
+    require 'json'
+
+    def buscar
+        # debugger
+        @cep = cep_params[:cep]
+        url = "https://viacep.com.br/ws/#{@cep}/json/"
+
+        retorno = JSON.parse(Net::HTTP.get(URI(url)))
+        if retorno["erro"]
+            render json: {cidade: retorno["O CEP não existe!"]}, status: :ok
+        else
+            estado = Estado.find_or_initialize_by(uf: retorno["uf"])
+            estado.save
+
+            cidade = Cidade.find_or_initialize_by(nome: retorno["localidade"], estado_id: estado.id)
+            cidade.save
+
+            endereco = Endereco.find_or_initialize_by(cep: retorno["cep"])
+            endereco.cidade = retorno["cidade"]
+            endereco.logradouro = retorno["logradouro"]
+            endereco.bairro = retorno["bairro"]
+            endereco.complemento = retorno["complemento"]
+            endereco.save
+
+            render json endereco.to_json, status: :ok
+        end
+
+        #endereco = Endereco.new
+
+    rescue => exception
+        render json: {erro: "Ligar no suporte"}, status: :ok
+    end
+
+    private
+
+    def cep_params
+        params.permit(:cep)
+    end
+end
